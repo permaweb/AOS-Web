e0d7706a1ae35e235a1643e746492b43ac6b7737
kaihenthorniwane <55228611+kaihenthorniwane@users.noreply.github.com>
2024-03-27 13:52:51 -0700

    add resizability

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -3,15 +3,45 @@ import SearchIcon from "./components/icons/SearchIcon";
 import SmallButton from "./components/SmallButton";
 import SmallPlus from "./components/icons/SmallPlus";
 import EmptyBoxIcon from "./components/icons/EmptyBoxIcon";
+import { useEffect, useRef, useState } from "react";
 
 function HomePage() {
+  const resizeElement = useRef<HTMLDivElement>(null);
+  const [sideBarWidth, setSideBarWidth] = useState<number>(250);
+
+  useEffect(() => {
+    if (resizeElement.current) {
+      const resizeSidebar = (e: MouseEvent) => {
+        if (resizeElement.current) {
+          setSideBarWidth(e.clientX);
+        }
+      };
+      const stopResize = () => {
+        window.removeEventListener("mousemove", resizeSidebar, false);
+        window.removeEventListener("mouseup", stopResize, false);
+      };
+      const initResize = () => {
+        window.addEventListener("mousemove", resizeSidebar, false);
+        window.addEventListener("mouseup", stopResize, false);
+      };
+      resizeElement.current.addEventListener("mousedown", initResize, false);
+    }
+  }, [resizeElement]);
+
   return (
     <div className="flex flex-col fixed left-0 top-0 right-0 bottom-0 font-roboto-mono text-primary-dark-color bg-bg-color text-sm">
       <div className="flex justify-between p-5 border-b-1 border-light-gray-color">
         <AOSLogo />
       </div>
-      <div className="grid grid-cols-[11.5rem,auto] flex-grow">
-        <div className="flex flex-col gap-5 py-5 border-r-1 border-light-gray-color">
+      <div className="grid grid-cols-[auto,1fr] flex-grow">
+        <div
+          className="flex flex-col relative gap-5 py-5 border-r-1 border-light-gray-color"
+          style={{ width: sideBarWidth }}
+        >
+          <div
+            ref={resizeElement}
+            className="absolute -right-2 top-0 bottom-0 w-4 hover:cursor-col-resize select-none"
+          />
           <div className="flex flex-col gap-2.5 px-5">
             <span className="uppercase">MY PROCESSES</span>
             <div className="flex flex-col gap-1.5">
