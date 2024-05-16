8b6405d32f54c88424af3d34ad8e8880b25d4051
kaihenthorniwane <55228611+kaihenthorniwane@users.noreply.github.com>
2024-05-15 18:18:28 -0700

    update vite config

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
-/// <reference types="vite/client" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "",
+  plugins: [react()],
+});
